bf0503ca629c6d04e2067ee878ff1157498e24d1
hkanuri <hkanuri@lululemon.com>
2021-02-22 20:47:59 -0600

    added save buton function

--- script.js
+++ script.js
@@ -22,3 +22,32 @@ function setPlanner() {
         }
     });
 }
+
+setPlanner();
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+
+    localStorage.setItem(time, schedule);
+});
+
+function pastPresentFuture() {
+    hour = time.hours();
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("id"));
+
+        if (thisHour > hour) {
+            $(this).addClass("future")
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+pastPresentFuture();
